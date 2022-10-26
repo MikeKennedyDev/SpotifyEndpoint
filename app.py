276e7373ea61b7f2c5985b8db5695ece3fc20c9a
unknown <mike.kennedy.dev@gmail.com>
2022-10-26 16:08:06 -0400

    Json results returned

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    print('Request for index page received')
+    return render_template('index.html')
+
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return render_template('hello.html', name=name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
+
 
 @app.route('/testioli')
 def test():
-    return "Hello testioli"
+    return jsonify("Hello testioli")
+
 
 @app.get('/gettest')
 def gettest():
-    return "this is what you got"
+    return jsonify("Get this")
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
